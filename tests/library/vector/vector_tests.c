8a4fb7eaffae12b9c5f449ab1aff2c807da28c7e
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2022-12-20 14:10:23 +0000

    Add vector swap function tests #20

--- tests/library/vector/vector_tests.c
+++ tests/library/vector/vector_tests.c
@@ -181,3 +181,60 @@ Test(vector_empty, vector_empty_false)
     vector.emplace_back(&vector, &data);
     cr_assert_eq(vector.empty(&vector), false);
 }
+
+Test(vector_swap, vector_swap_valid)
+{
+    vector_t vector;
+    int rvalue = vector_constructor(&vector, sizeof(int), 10);
+    int data = 10;
+
+    cr_assert_eq(rvalue, 0);
+    for (int i = 0; i < 10; i++) {
+        vector.emplace_back(&vector, &data);
+        data++;
+    }
+    vector.swap(&vector, 0, 1);
+    rvalue = *(int *)vector.at(&vector, 0);
+    cr_assert_eq(rvalue, 11);
+    rvalue = *(int *)vector.at(&vector, 1);
+    cr_assert_eq(rvalue, 10);
+    vector.destructor(&vector);
+}
+
+Test(vector_swap, vector_swap_element1_invalid_size)
+{
+    vector_t vector;
+    int rvalue = vector_constructor(&vector, sizeof(int), 10);
+    int data = 10;
+
+    cr_assert_eq(rvalue, 0);
+    for (int i = 0; i < 10; i++) {
+        vector.emplace_back(&vector, &data);
+        data++;
+    }
+    vector.swap(&vector, 100, 1);
+    rvalue = *(int *)vector.at(&vector, 0);
+    cr_assert_eq(rvalue, 10);
+    rvalue = *(int *)vector.at(&vector, 1);
+    cr_assert_eq(rvalue, 11);
+    vector.destructor(&vector);
+}
+
+Test(vector_swap, vector_swap_element2_invalid_size)
+{
+    vector_t vector;
+    int rvalue = vector_constructor(&vector, sizeof(int), 10);
+    int data = 10;
+
+    cr_assert_eq(rvalue, 0);
+    for (int i = 0; i < 10; i++) {
+        vector.emplace_back(&vector, &data);
+        data++;
+    }
+    vector.swap(&vector, 1, 100);
+    rvalue = *(int *)vector.at(&vector, 0);
+    cr_assert_eq(rvalue, 10);
+    rvalue = *(int *)vector.at(&vector, 1);
+    cr_assert_eq(rvalue, 11);
+    vector.destructor(&vector);
+}

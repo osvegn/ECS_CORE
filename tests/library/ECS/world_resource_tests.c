9177c0f8c9de9e622ef4a7cc2ad220336ff34c5a
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2022-12-29 23:15:31 +0000

    Create world_remove_resource tests #28

--- tests/library/ECS/world_resource_tests.c
+++ tests/library/ECS/world_resource_tests.c
@@ -43,3 +43,32 @@ Test(world_add_resource, world_add_resource_success_with_two_elements)
     cr_assert_eq(add_resource(&world, resource), 0);
     cr_assert_eq(world.resource_list.size(&world.entity_list), 2);
 }
+
+Test(world_remove_resource, world_remove_resource)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.id = 0;
+    vector_constructor(&world.resource_list, sizeof(int), 0);
+    cr_assert_eq(add_resource(&world, resource), 0);
+    resource.id = 1;
+    cr_assert_eq(add_resource(&world, resource), 0);
+    cr_assert_eq(remove_resource(&world, resource), 0);
+    cr_assert_eq(world.resource_list.size(&world.entity_list), 1);
+}
+
+Test(world_remove_resource, world_remove_resource_failure)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.id = 0;
+    vector_constructor(&world.resource_list, sizeof(int), 0);
+    cr_assert_eq(add_resource(&world, resource), 0);
+    resource.id = 1;
+    cr_assert_eq(add_resource(&world, resource), 0);
+    cr_assert_eq(remove_resource(&world, resource), 0);
+    cr_assert_eq(remove_resource(&world, resource), -1);
+    cr_assert_eq(world.resource_list.size(&world.entity_list), 1);
+}

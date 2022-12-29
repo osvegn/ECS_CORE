3c158d70ca3964ddc871465cbeb9df67894f8374
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2022-12-29 23:12:51 +0000

    Create world_add_resource tests #28

---
+++ tests/library/ECS/world_resource_tests.c
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2022
+** our_rpg [Codespaces]
+** File description:
+** world_resource_tests
+*/
+
+#include <criterion/criterion.h>
+#include "world.h"
+
+Test(world_add_resource, world_add_resource)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.id = 0;
+    vector_constructor(&world.resource_list, sizeof(int), 0);
+    cr_assert_eq(add_resource(&world, resource), 0);
+    cr_assert_eq(world.resource_list.size(&world.entity_list), 1);
+}
+
+Test(world_add_resource, world_add_resource_failure)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.id = 0;
+    vector_constructor(&world.resource_list, sizeof(int), 0);
+    cr_assert_eq(add_resource(&world, resource), 0);
+    cr_assert_eq(add_resource(&world, resource), -1);
+    cr_assert_eq(world.resource_list.size(&world.entity_list), 1);
+}
+
+Test(world_add_resource, world_add_resource_success_with_two_elements)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.id = 0;
+    vector_constructor(&world.resource_list, sizeof(int), 0);
+    cr_assert_eq(add_resource(&world, resource), 0);
+    resource.id = 1;
+    cr_assert_eq(add_resource(&world, resource), 0);
+    cr_assert_eq(world.resource_list.size(&world.entity_list), 2);
+}

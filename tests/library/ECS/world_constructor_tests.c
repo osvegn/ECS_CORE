9bcd4e36223823995d28dc7b9d1d964aa649362a
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2022-12-30 13:48:42 +0000

    Add world constructor and destructor tests Create ECS world constructor #32

---
+++ tests/library/ECS/world_constructor_tests.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** our_rpg [Codespaces]
+** File description:
+** world_constructor_tests
+*/
+
+#include <criterion/criterion.h>
+#include "world.h"
+
+Test(world_constructor, world_constructor)
+{
+    world_t world;
+
+    cr_assert_eq(world_constructor(&world), 0);
+}

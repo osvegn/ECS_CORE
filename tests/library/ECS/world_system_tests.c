231fdbc53cba58b74ff1591e5a7d33864641a72e
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2022-12-30 13:38:50 +0000

    Fix world system tests #28

--- tests/library/ECS/world_system_tests.c
+++ tests/library/ECS/world_system_tests.c
@@ -16,7 +16,7 @@ Test(world_add_system, world_add_system)
     system.type = 0;
     vector_constructor(&world.system_list, sizeof(int), 0);
     cr_assert_eq(add_system(&world, system), 0);
-    cr_assert_eq(world.system_list.size(&world.resource_list), 1);
+    cr_assert_eq(world.system_list.size(&world.system_list), 1);
 }
 
 Test(world_add_system, world_add_system_failure)
@@ -28,7 +28,7 @@ Test(world_add_system, world_add_system_failure)
     vector_constructor(&world.system_list, sizeof(int), 0);
     cr_assert_eq(add_system(&world, system), 0);
     cr_assert_eq(add_system(&world, system), -1);
-    cr_assert_eq(world.system_list.size(&world.resource_list), 1);
+    cr_assert_eq(world.system_list.size(&world.system_list), 1);
 }
 
 Test(world_add_system, world_add_system_success_with_two_elements)
@@ -41,7 +41,7 @@ Test(world_add_system, world_add_system_success_with_two_elements)
     cr_assert_eq(add_system(&world, system), 0);
     system.type = 1;
     cr_assert_eq(add_system(&world, system), 0);
-    cr_assert_eq(world.system_list.size(&world.resource_list), 2);
+    cr_assert_eq(world.system_list.size(&world.system_list), 2);
 }
 
 Test(world_remove_system, world_remove_system)
@@ -55,7 +55,7 @@ Test(world_remove_system, world_remove_system)
     system.type = 1;
     cr_assert_eq(add_system(&world, system), 0);
     cr_assert_eq(remove_system(&world, system), 0);
-    cr_assert_eq(world.system_list.size(&world.resource_list), 1);
+    cr_assert_eq(world.system_list.size(&world.system_list), 1);
 }
 
 Test(world_remove_system, world_remove_system_failure)
@@ -70,5 +70,5 @@ Test(world_remove_system, world_remove_system_failure)
     cr_assert_eq(add_system(&world, system), 0);
     cr_assert_eq(remove_system(&world, system), 0);
     cr_assert_eq(remove_system(&world, system), -1);
-    cr_assert_eq(world.system_list.size(&world.resource_list), 1);
+    cr_assert_eq(world.system_list.size(&world.system_list), 1);
 }

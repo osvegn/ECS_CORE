b64799c9de299b9927460eb1722dc1c0231dbb48
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2022-12-30 13:26:00 +0000

    Create tests for add and remove function of world system list #28

---
+++ tests/library/ECS/world_system_tests.c
@@ -0,0 +1,74 @@
+/*
+** EPITECH PROJECT, 2022
+** our_rpg [Codespaces]
+** File description:
+** world_system_tests
+*/
+
+#include <criterion/criterion.h>
+#include "world.h"
+
+Test(world_add_system, world_add_system)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    vector_constructor(&world.system_list, sizeof(int), 0);
+    cr_assert_eq(add_system(&world, system), 0);
+    cr_assert_eq(world.system_list.size(&world.resource_list), 1);
+}
+
+Test(world_add_system, world_add_system_failure)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    vector_constructor(&world.system_list, sizeof(int), 0);
+    cr_assert_eq(add_system(&world, system), 0);
+    cr_assert_eq(add_system(&world, system), -1);
+    cr_assert_eq(world.system_list.size(&world.resource_list), 1);
+}
+
+Test(world_add_system, world_add_system_success_with_two_elements)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    vector_constructor(&world.system_list, sizeof(int), 0);
+    cr_assert_eq(add_system(&world, system), 0);
+    system.type = 1;
+    cr_assert_eq(add_system(&world, system), 0);
+    cr_assert_eq(world.system_list.size(&world.resource_list), 2);
+}
+
+Test(world_remove_system, world_remove_system)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    vector_constructor(&world.system_list, sizeof(int), 0);
+    cr_assert_eq(add_system(&world, system), 0);
+    system.type = 1;
+    cr_assert_eq(add_system(&world, system), 0);
+    cr_assert_eq(remove_system(&world, system), 0);
+    cr_assert_eq(world.system_list.size(&world.resource_list), 1);
+}
+
+Test(world_remove_system, world_remove_system_failure)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    vector_constructor(&world.system_list, sizeof(int), 0);
+    cr_assert_eq(add_system(&world, system), 0);
+    system.type = 1;
+    cr_assert_eq(add_system(&world, system), 0);
+    cr_assert_eq(remove_system(&world, system), 0);
+    cr_assert_eq(remove_system(&world, system), -1);
+    cr_assert_eq(world.system_list.size(&world.resource_list), 1);
+}

4b5f4fe6907221372b5d2307e0032e9f3370fcf7
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2023-01-02 13:14:23 +0000

    Create vector2i structure containing x/y values #35

---
+++ includes/vector2i.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** our_rpg [Codespaces]
+** File description:
+** vector2i
+*/
+
+#ifndef VECTOR2I_H_
+#define VECTOR2I_H_
+
+typedef struct vector2i_s {
+    int x;
+    int y;
+} vector2i_t;
+
+#endif /* !VECTOR2I_H_ */

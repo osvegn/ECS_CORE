9ebc790b43a2385980d858d66a5bdc90e30176f0
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2023-01-02 14:03:31 +0000

    Create tests for contains entity #29

--- tests/library/ECS/world_entity_tests.c
+++ tests/library/ECS/world_entity_tests.c
@@ -77,3 +77,24 @@ Test(world_add_entity, world_remove_entity_failure)
     cr_assert_eq(remove_entity(&world, entity), -1);
     cr_assert_eq(world.entity_list.size(&world.entity_list), 1);
 }
+
+Test(world_contains_entity, world_contains_entity_success)
+{
+    world_t world;
+    entity_t entity;
+
+    entity.id = 0;
+    world_constructor(&world);
+    add_entity(&world, entity);
+    cr_assert_eq(contains_entity(&world, entity), true);
+}
+
+Test(world_contains_entity, world_contains_entity_failure)
+{
+    world_t world;
+    entity_t entity;
+
+    entity.id = 0;
+    world_constructor(&world);
+    cr_assert_eq(contains_entity(&world, entity), false);
+}

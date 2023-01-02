dfe5167bb4a9df7b471918b80a9532cdf79402be
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2023-01-02 13:22:00 +0000

    Create position component #35

---
+++ includes/components.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** our_rpg [Codespaces]
+** File description:
+** components
+*/
+
+#ifndef COMPONENTS_H_
+#define COMPONENTS_H_
+
+typedef enum component_type_e {
+    UNDEFINED,
+    POSITION,
+    MAX_VALUE
+} component_type_t;
+
+#endif /* !COMPONENTS_H_ */
--- sources/Component/position.c
+++ sources/Component/position.c
@@ -6,32 +6,33 @@
 */
 
 #include "component.h"
+#include "components.h"
+#include "vector2i.h"
 #include <stdlib.h>
 
-component_t *create_position(sfVector2f pos)
+component_t *create_position(vector2i_t pos)
 {
     component_t *component = malloc(sizeof(component_t));
-    sfVector2f *position = malloc(sizeof(sfVector2f));
+    vector2i_t *position = malloc(sizeof(vector2i_t));
 
     if (!component || !position)
         return NULL;
     component->type = POSITION;
-    component->data = NULL;
     position->x = pos.x;
     position->y = pos.y;
     component->data = (void *)position;
     return component;
 }
 
-void set_position(component_t *component, const sfVector2f pos)
+void set_position(component_t *component, const vector2i pos)
 {
-    sfVector2f *position = component->data;
+    vector2i_t *position = component->data;
 
     position->x = pos.x;
     position->y = pos.y;
 }
 
-sfVector2f *get_position(const component_t *component)
+vector2i_t *get_position(const component_t *component)
 {
-    return (sfVector2f *)component->data;
+    return (vector2i_t *)component->data;
 }

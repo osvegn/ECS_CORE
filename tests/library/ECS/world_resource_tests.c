8f985737cff6a8fdfd370c6a1965621ce4e8611a
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2023-01-02 14:09:23 +0000

    Create tests for contains resource #29

--- tests/library/ECS/world_resource_tests.c
+++ tests/library/ECS/world_resource_tests.c
@@ -72,3 +72,24 @@ Test(world_remove_resource, world_remove_resource_failure)
     cr_assert_eq(remove_resource(&world, resource), -1);
     cr_assert_eq(world.resource_list.size(&world.resource_list), 1);
 }
+
+Test(world_contains_resource, world_contains_resource_success)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.type = 0;
+    world_constructor(&world);
+    add_resource(&world, resource);
+    cr_assert_eq(contains_resource(&world, resource), true);
+}
+
+Test(world_contains_resource, world_contains_resource_failure)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.type = 0;
+    world_constructor(&world);
+    cr_assert_eq(contains_resource(&world, resource), false);
+}

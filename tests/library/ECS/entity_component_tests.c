67ed5701f6d43e39f9ab0b09f5661ce103bf2230
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2023-01-03 14:51:26 +0000

    Add tests for entity manipulation #30

---
+++ tests/library/ECS/entity_component_tests.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2023
+** our_rpg [Codespaces]
+** File description:
+** entity_component_tests
+*/
+
+#include "entity.h"
+#include "component.h"
+
+Test(entity_component, test_entity_constructor)
+{
+    entity_t entity;
+
+    cr_assert_eq(entity_constructor(&entity), 0);
+}
+
+
+Test(entity_component, test_entity_add_component)
+{
+    entity_t entity;
+
+    entity_constructor(&entity);
+    cr_assert_eq(add_component(&entity, (component_t){0, 0}), 0);
+}

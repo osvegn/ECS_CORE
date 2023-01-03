c0c887ef4a1cc5789bf0c506b279ede72d2a3e06
Thomas Prud'homme <thomas.prudhomme@epitech.eu>
2023-01-03 17:38:21 +0000

    Add tests for join_entities function #30

--- tests/library/ECS/world_entity_tests.c
+++ tests/library/ECS/world_entity_tests.c
@@ -137,3 +137,33 @@ Test(world_join_entities, world_join_entities_3)
     add_entity(&world, entity);
     cr_assert_eq(join_entities(&world, &vector, 1, 1), expected_value);
 }
+
+Test(world_join_entities, world_join_entities_4)
+{
+    world_t world;
+    vector_t vector;
+    entity_t entity;
+    
+    world_constructor(&world);
+    entity_constructor(&entity);
+    add_component(&entity, (component_t){1, 0});
+    add_entity(&world, entity);
+    entity_constructor(&entity);
+    add_component(&entity, (component_t){2, 0});
+    add_entity(&world, entity);
+    cr_assert_eq(join_entities(&world, &vector, 2, 1, 2), 0);
+}
+
+Test(world_join_entities, world_join_entities_5)
+{
+    world_t world;
+    vector_t vector;
+    entity_t entity;
+    
+    world_constructor(&world);
+    entity_constructor(&entity);
+    add_component(&entity, (component_t){1, 0});
+    add_component(&entity, (component_t){2, 0});
+    add_entity(&world, entity);
+    cr_assert_eq(join_entities(&world, &vector, 2, 1, 2), 1);
+}

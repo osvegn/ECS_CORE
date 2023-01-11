a906eb5b177d921147d4cc8dc8b694ff8f721d69
osvegn <thomas.prudhomme@epitech.eu>
2023-01-11 14:27:43 +0100

    Add tests for world_system #30

--- tests/library/ECS/world_system_tests.c
+++ tests/library/ECS/world_system_tests.c
@@ -26,7 +26,7 @@ Test(world_add_system, world_add_system_failure)
 
     system.type = 0;
     vector_constructor(&world.system_list, sizeof(int), 0);
-    cr_assert_eq(add_system(&world, system), 0);
+    add_system(&world, system);
     cr_assert_eq(add_system(&world, system), -1);
     cr_assert_eq(world.system_list.size(&world.system_list), 1);
 }
@@ -38,7 +38,7 @@ Test(world_add_system, world_add_system_success_with_two_elements)
 
     system.type = 0;
     vector_constructor(&world.system_list, sizeof(int), 0);
-    cr_assert_eq(add_system(&world, system), 0);
+    add_system(&world, system);
     system.type = 1;
     cr_assert_eq(add_system(&world, system), 0);
     cr_assert_eq(world.system_list.size(&world.system_list), 2);
@@ -51,9 +51,9 @@ Test(world_remove_system, world_remove_system)
 
     system.type = 0;
     vector_constructor(&world.system_list, sizeof(int), 0);
-    cr_assert_eq(add_system(&world, system), 0);
+    add_system(&world, system);
     system.type = 1;
-    cr_assert_eq(add_system(&world, system), 0);
+    add_system(&world, system);
     cr_assert_eq(remove_system(&world, system), 0);
     cr_assert_eq(world.system_list.size(&world.system_list), 1);
 }
@@ -65,11 +65,40 @@ Test(world_remove_system, world_remove_system_failure)
 
     system.type = 0;
     vector_constructor(&world.system_list, sizeof(int), 0);
-    cr_assert_eq(add_system(&world, system), 0);
+    add_system(&world, system);
     system.type = 1;
-    cr_assert_eq(add_system(&world, system), 0);
-    cr_assert_eq(remove_system(&world, system), 0);
+    add_system(&world, system);
+    remove_system(&world, system);
     cr_assert_eq(remove_system(&world, system), -1);
+    cr_assert_eq(world.system_list.size(&world.system_list), 1);
+}
+
+Test(world_remove_system, world_remove_system_by_type)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    vector_constructor(&world.system_list, sizeof(int), 0);
+    add_system(&world, system);
+    system.type = 1;
+    add_system(&world, system);
+    cr_assert_eq(remove_system_by_type(&world, 0), 0);
+    cr_assert_eq(world.system_list.size(&world.system_list), 1);
+}
+
+Test(world_remove_system, world_remove_system_by_type_failure)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    vector_constructor(&world.system_list, sizeof(int), 0);
+    add_system(&world, system);
+    system.type = 1;
+    add_system(&world, system);
+    remove_system_by_type(&world, 0);
+    cr_assert_eq(remove_system_by_type(&world, 0), -1);
     cr_assert_eq(world.system_list.size(&world.system_list), 1);
 }
 
@@ -92,4 +121,70 @@ Test(world_contains_system, world_contains_system_failure)
     system.type = 0;
     world_constructor(&world);
     cr_assert_eq(contains_system(&world, system), false);
+}
+
+Test(world_contains_system, world_contains_system_by_type_success)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    world_constructor(&world);
+    add_system(&world, system);
+    cr_assert_eq(contains_system_by_type(&world, 0), true);
+}
+
+Test(world_contains_system, world_contains_system_by_type_failure)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    world_constructor(&world);
+    add_system(&world, system);
+    cr_assert_eq(contains_system_by_type(&world, 1), true);
+}
+
+Test(wolrd_get_system, world_get_system_success)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    world_constructor(&world);
+    add_system(&world, system);
+    cr_assert_neq(get_system(&world, (system_t){0, 0}), 0);
+}
+
+Test(wolrd_get_system, world_get_system_failure)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    world_constructor(&world);
+    add_system(&world, system);
+    cr_assert_eq(get_system(&world, (system_t){1, 0}), 0);
+}
+
+Test(wolrd_get_system, world_get_system_by_type_success)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    world_constructor(&world);
+    add_system(&world, system);
+    cr_assert_neq(get_system_by_type(&world, 0), 0);
+}
+
+Test(wolrd_get_system, world_get_system_by_type_failure)
+{
+    world_t world;
+    system_t system;
+
+    system.type = 0;
+    world_constructor(&world);
+    add_system(&world, system);
+    cr_assert_eq(get_system_by_type(&world, 1), 0);
 }

cbbb9e5f98848a3ad2e0328cf10d18f7cbf8cc09
osvegn <thomas.prudhomme@epitech.eu>
2023-01-11 13:55:46 +0100

    Add tests for world resource function #30

--- tests/library/ECS/world_resource_tests.c
+++ tests/library/ECS/world_resource_tests.c
@@ -73,6 +73,35 @@ Test(world_remove_resource, world_remove_resource_failure)
     cr_assert_eq(world.resource_list.size(&world.resource_list), 1);
 }
 
+Test(world_remove_resource, world_remove_resource_by_type)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.type = 0;
+    vector_constructor(&world.resource_list, sizeof(int), 0);
+    cr_assert_eq(add_resource(&world, resource), 0);
+    resource.type = 1;
+    cr_assert_eq(add_resource(&world, resource), 0);
+    cr_assert_eq(remove_resource_by_type(&world, 0), 0);
+    cr_assert_eq(world.resource_list.size(&world.resource_list), 1);
+}
+
+Test(world_remove_resource, world_remove_resource_by_type_failure)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.type = 0;
+    vector_constructor(&world.resource_list, sizeof(int), 0);
+    cr_assert_eq(add_resource(&world, resource), 0);
+    resource.type = 1;
+    cr_assert_eq(add_resource(&world, resource), 0);
+    cr_assert_eq(remove_resource_by_type(&world, 0), 0);
+    cr_assert_eq(remove_resource_by_type(&world, 0), -1);
+    cr_assert_eq(world.resource_list.size(&world.resource_list), 1);
+}
+
 Test(world_contains_resource, world_contains_resource_success)
 {
     world_t world;
@@ -92,4 +121,70 @@ Test(world_contains_resource, world_contains_resource_failure)
     resource.type = 0;
     world_constructor(&world);
     cr_assert_eq(contains_resource(&world, resource), false);
+}
+
+Test(world_contains_resource, world_contains_resource_by_type_success)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.type = 0;
+    world_constructor(&world);
+    add_resource(&world, resource);
+    cr_assert_eq(contains_resource_by_type(&world, 0), true);
+}
+
+Test(world_contains_resource, world_contains_resource_by_type_failure)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.type = 0;
+    world_constructor(&world);
+    add_resource(&world, resource);
+    cr_assert_eq(contains_resource_by_type(&world, 1), false);
+}
+
+Test(world_get_resource, world_get_resource_success)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.type = 0;
+    world_constructor(&world);
+    add_resource(&world, resource);
+    cr_assert_neq(get_resource(&world, (resource_t){0, 0}), 0);
+}
+
+Test(world_get_resource, world_get_resource_failure)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.type = 0;
+    world_constructor(&world);
+    add_resource(&world, resource);
+    cr_assert_eq(get_resource(&world, (resource_t){1, 0}), 0);
+}
+
+Test(world_get_resource, world_get_resource_by_id_success)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.type = 0;
+    world_constructor(&world);
+    add_resource(&world, resource);
+    cr_assert_neq(get_resource_by_type(&world, 0), 0);
+}
+
+Test(world_get_resource, world_get_resource_by_id_failure)
+{
+    world_t world;
+    resource_t resource;
+
+    resource.type = 0;
+    world_constructor(&world);
+    add_resource(&world, resource);
+    cr_assert_eq(get_resource_by_type(&world, 1), 0);
 }

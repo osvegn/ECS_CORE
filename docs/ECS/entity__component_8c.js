1ea0011ec414c6ab6f9960ea2c57db3ab6ed63c0
osvegn <osvegn@users.noreply.github.com>
2023-03-09 14:30:31 +0000

    Deploying to gh-pages from @ osvegn/our_rpg@d91cf3dc6d15dd2b9336ca45cb7ce6e32cb84f7d 🚀

--- docs/ECS/entity__component_8c.js
+++ docs/ECS/entity__component_8c.js
@@ -1,9 +1,10 @@
 var entity__component_8c =
 [
-    [ "entity_add_component", "entity__component_8c.html#ac680b44c87b77731500b9cf1014fdfe0", null ],
+    [ "entity_add_component", "entity__component_8c.html#ae9c4710107a012ef60c252072a5dd5b9", null ],
     [ "entity_constructor", "entity__component_8c.html#aa4d524c8b6d170b4b513842e4d858c63", null ],
-    [ "entity_contains_component", "entity__component_8c.html#ac2d6e5cb2d530516f7a19ca8bc74646e", null ],
+    [ "entity_contains_component", "entity__component_8c.html#a047dda0071c76403873c41814fe77c20", null ],
+    [ "entity_contains_component_by_type", "entity__component_8c.html#a0fc2f14b15d533482f2180f149afc72b", null ],
     [ "entity_destructor", "entity__component_8c.html#aa52c87ef09bcc4c44121c235b1a77244", null ],
     [ "entity_get_component", "entity__component_8c.html#afc477fdf5c88e639c50f163067c84053", null ],
-    [ "entity_remove_component", "entity__component_8c.html#a11e52ab05727b14f89160bea791620ef", null ]
+    [ "entity_remove_component", "entity__component_8c.html#ac0e9402cb9be7aa3d6e10cb7e81c3124", null ]
 ];

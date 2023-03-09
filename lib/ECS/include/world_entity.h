885e2932181f02ece4c7262d970d3afa7c4979a2
osvegn <thomas.prudhomme@epitech.eu>
2023-03-09 09:53:39 +0100

    Update world_entity functions

--- lib/ECS/include/world_entity.h
+++ lib/ECS/include/world_entity.h
@@ -17,19 +17,19 @@
 /// @param world The world to add the entity to.
 /// @param entity The entity to add to the world.
 /// @return 0, or -1 if the entity already exists.
-int world_add_entity(world_t *world, entity_t entity);
+int world_add_entity(world_t *world, entity_t *entity);
 
 /// @brief It removes an entity from the world
 /// @param world The world that the entity is in.
 /// @param entity The entity to remove from the world.
 /// @return 0, or -1 if the entity isn't found.
-int world_remove_entity(world_t *world, entity_t entity);
+int world_remove_entity(world_t *world, entity_t *entity);
 
 /// @brief It returns true if the entity is in the world, false otherwise
 /// @param world The world that the entity is in.
 /// @param entity The entity to check for.
 /// @return A boolean value.
-bool world_contains_entity(world_t *world, entity_t entity);
+bool world_contains_entity(world_t *world, entity_t *entity);
 
 /// @brief Get an entity from the world entity list by his id.
 /// @param world The world on which find the entity.

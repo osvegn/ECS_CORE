60c0b9ea3b2aed83407979a4a773c7a6a60d3d8d
osvegn <thomas.prudhomme@epitech.eu>
2023-03-10 09:08:24 +0100

    Add documentation for windows resource usage

--- sources/Resource/window_resource.c
+++ sources/Resource/window_resource.c
@@ -10,6 +10,27 @@
 #include "resources.h"
 #include "raylib.h"
 
+/// @brief This function is used to create a window resource
+/// @param resource The resource to create
+/// @return 0 if the resource was created, -1 otherwise
+/// @details **Example**
+/// @code
+///    int main(void)
+///    {
+///        resource_t window;
+///
+///        if (window_constructor(&window) == -1)
+///            return 84;
+///        while (!WindowShouldClose()) {
+///            BeginDrawing();
+///            ClearBackground(RAYWHITE);
+///            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+///            EndDrawing();
+///        }
+///        window_destructor(&window);
+///        return 0;
+///    }
+/// @endcode
 int window_constructor(resource_t *resource)
 {
     window_t window = {800, 450, "Our RPG", 60};
@@ -17,6 +38,31 @@ int window_constructor(resource_t *resource)
     return window_copy_constructor(resource, &window);
 }
 
+/// @brief This function is used to create a window resource
+/// @param resource The resource to create
+/// @param width The width of the window
+/// @param height The height of the window
+/// @param title The title of the window
+/// @param fps The fps of the window
+/// @return 0 if the resource was created, -1 otherwise
+/// @details **Example**
+/// @code
+///    int main(void)
+///    {
+///        resource_t window;
+///
+///        if (window_constructor_with_params(&window, 800, 450, "Our RPG", 60) == -1)
+///            return 84;
+///        while (!WindowShouldClose()) {
+///            BeginDrawing();
+///            ClearBackground(RAYWHITE);
+///            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+///            EndDrawing();
+///        }
+///        window_destructor(&window);
+///        return 0;
+///    }
+/// @endcode
 int window_constructor_with_params(resource_t *resource, unsigned int width, unsigned int height, char *title, unsigned int fps)
 {
     window_t window = {width, height, title, fps};
@@ -24,6 +70,29 @@ int window_constructor_with_params(resource_t *resource, unsigned int width, uns
     return window_copy_constructor(resource, &window);
 }
 
+/// @brief This function is used to create a window resource
+/// @param resource The resource to create
+/// @param w The window to copy
+/// @return 0 if the resource was created, -1 otherwise
+/// @details **Example**
+/// @code
+///    int main(void)
+///    {
+///        resource_t window;
+///        window_t w = {800, 450, "Our RPG", 60};
+///
+///        if (window_copy_constructor(&window, &w) == -1)
+///            return 84;
+///        while (!WindowShouldClose()) {
+///            BeginDrawing();
+///            ClearBackground(RAYWHITE);
+///            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+///            EndDrawing();
+///        }
+///        window_destructor(&window);
+///        return 0;
+///    }
+/// @endcode
 int window_copy_constructor(resource_t *resource, window_t *w)
 {
     window_t *window = malloc(sizeof(window_t));
@@ -41,6 +110,20 @@ int window_copy_constructor(resource_t *resource, window_t *w)
     return 0;
 }
 
+/// @brief This function is used to close the window and free the memory allocated for the resource
+/// @param resource The resource to free
+/// @details **Example**
+/// @code
+///    int main(void)
+///    {
+///        resource_t window;
+///
+///        if (window_constructor(&window) == -1)
+///            return 84;
+///        window_destructor(&window);
+///        return 0;
+///    }
+/// @endcode
 void window_destructor(resource_t *resource)
 {
     CloseWindow();

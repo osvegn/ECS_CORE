24f835513a54b0b64442b2b921b89ded9383e552
osvegn <thomas.prudhomme@epitech.eu>
2023-03-10 18:54:49 +0100

    Remove useless comment in main

--- sources/main.c
+++ sources/main.c
@@ -11,25 +11,6 @@
 #include "world_system.h"
 #include "systems.h"
 
-// #include "raylib.h"
-// int main(void)
-// {
-//     const int screenWidth = 800;
-//     const int screenHeight = 450;
-
-//     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-//     SetTargetFPS(60);
-//     while (!WindowShouldClose())
-//     {
-//         BeginDrawing();
-//             ClearBackground(RAYWHITE);
-//             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-//         EndDrawing();
-//     }
-//     CloseWindow();
-//     return 0;
-// }
-
 int main(void)
 {
     world_t world;

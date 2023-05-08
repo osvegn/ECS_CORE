51e8121962fab387efaeafa411d96787d59bb907
osvegn <thomas.prudhomme@epitech.eu>
2023-05-08 23:45:50 +0200

    Fix component size (key in json file was invalid) #67

--- sources/Component/component_size.c
+++ sources/Component/component_size.c
@@ -43,8 +43,8 @@ int component_size_set(component_t *component, void *data)
 
     if (!component_is_size(component) || !json)
         return -1;
-    size.x = json_object_get_int(json_object_object_get(json, "x"));
-    size.y = json_object_get_int(json_object_object_get(json, "y"));
+    size.x = json_object_get_int(json_object_object_get(json, "width"));
+    size.y = json_object_get_int(json_object_object_get(json, "height"));
     json_object_put(json);
     memcpy(component->data, &size, sizeof(ecs_vector2i_t));
     return 0;

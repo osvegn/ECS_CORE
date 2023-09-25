df503e967e1206b8958128cf045c0903c330f6db
osvegn <thomas.prudhomme@epitech.eu>
2023-09-25 06:49:34 +0000

    Deploying to gh-pages from @ osvegn/ECS_CORE@f56dfc34a4d0b23a3b1d51a8c97b139b15df2c6b 🚀

--- docs/ECS_CORE/annotated_dup.js
+++ docs/ECS_CORE/annotated_dup.js
@@ -1,6 +1,7 @@
 var annotated_dup =
 [
     [ "ecs_color_s", "structecs__color__s.html", "structecs__color__s" ],
+    [ "ecs_vector2f_s", "structecs__vector2f__s.html", "structecs__vector2f__s" ],
     [ "ecs_vector2i_s", "structecs__vector2i__s.html", "structecs__vector2i__s" ],
     [ "window_s", "structwindow__s.html", "structwindow__s" ]
 ];
--- docs/ECS_CORE/search/all_3.js
+++ docs/ECS_CORE/search/all_3.js
@@ -4,6 +4,8 @@ var searchData=
   ['ecs_5fcolor_5fs_1',['ecs_color_s',['../structecs__color__s.html',1,'']]],
   ['ecs_5fcolor_5ft_2',['ecs_color_t',['../ecs__color_8h.html#a537da1a65a22b4686ee49661b9579552',1,'ecs_color.h']]],
   ['ecs_5fvector_2eh_3',['ecs_vector.h',['../ecs__vector_8h.html',1,'']]],
-  ['ecs_5fvector2i_5fs_4',['ecs_vector2i_s',['../structecs__vector2i__s.html',1,'']]],
-  ['ecs_5fvector2i_5ft_5',['ecs_vector2i_t',['../ecs__vector_8h.html#a638857d6847872a6ca80d693d508329b',1,'ecs_vector.h']]]
+  ['ecs_5fvector2f_5fs_4',['ecs_vector2f_s',['../structecs__vector2f__s.html',1,'']]],
+  ['ecs_5fvector2f_5ft_5',['ecs_vector2f_t',['../ecs__vector_8h.html#a317aa2111a18eefe9fde4b7cd14aac9f',1,'ecs_vector.h']]],
+  ['ecs_5fvector2i_5fs_6',['ecs_vector2i_s',['../structecs__vector2i__s.html',1,'']]],
+  ['ecs_5fvector2i_5ft_7',['ecs_vector2i_t',['../ecs__vector_8h.html#a638857d6847872a6ca80d693d508329b',1,'ecs_vector.h']]]
 ];
